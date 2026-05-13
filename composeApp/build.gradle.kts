08ff28d4255e557a315852499345594214a1588f
SOR <3272194354@qq.com>
2026-05-13 20:28:06 +0800

    fix: gradle settings

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -55,6 +55,7 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.material.icons.extended)
 
             // https://github.com/dosier/kodio
             implementation(libs.kodio.core)
