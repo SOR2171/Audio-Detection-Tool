ef00e17ff610a37d547161c16e8a71c8bd2e415a
SOR <3272194354@qq.com>
2026-05-14 21:10:28 +0800

    fix: Android microphone permission

--- composeApp/src/androidMain/kotlin/com/github/sor2171/audiodetectiontool/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/github/sor2171/audiodetectiontool/MainActivity.kt
@@ -1,22 +1,51 @@
 package com.github.sor2171.audiodetectiontool
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import space.kodio.core.Kodio
 import space.kodio.core.initialize
 
 class MainActivity : ComponentActivity() {
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // Permission granted
+        } else {
+            // Permission denied
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         Kodio.initialize(this)
-
+        checkAndRequestAudioPermission()
         setContent {
             App()
+        }
+    }
+
+    private fun checkAndRequestAudioPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // have permission, do nothing
+            }
+            else -> {
+                requestPermissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
+            }
         }
     }
 }
