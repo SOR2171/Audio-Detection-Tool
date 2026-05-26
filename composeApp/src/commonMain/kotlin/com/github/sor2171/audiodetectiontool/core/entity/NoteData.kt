8d6d8b48a6c7e5f0be1b155ec5f24f8e158065c5
SOR <3272194354@qq.com>
2026-05-26 13:46:34 +0800

    feature: frequency display

--- composeApp/src/commonMain/kotlin/com/github/sor2171/audiodetectiontool/core/entity/NoteData.kt
+++ composeApp/src/commonMain/kotlin/com/github/sor2171/audiodetectiontool/core/entity/NoteData.kt
@@ -1,6 +1,9 @@
 package com.github.sor2171.audiodetectiontool.core.entity
 
-data class NoteData(
+data class NoteData<T : Number>(
     val name: String,
-    val cent: Int
-)
+    val cent: T
+) {
+    fun intCent() = NoteData(name, cent.toInt())
+    fun floatCent() = NoteData(name, cent.toFloat())
+}
